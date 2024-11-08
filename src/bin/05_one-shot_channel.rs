9706c2544a5ae004d4c58d04d8d3b97b6d300c6a
Nick Kotsakidis <dev@zerc.dev>
2024-11-08 16:07:59 +0100

    chapter 5

---
+++ src/bin/05_one-shot_channel.rs
@@ -0,0 +1,73 @@
+use std::sync::atomic::Ordering::*;
+use std::thread;
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::{atomic::AtomicBool, Arc},
+};
+
+struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+pub struct Sender<T> {
+    channel: Arc<Channel<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
+    }
+}
+
+pub struct Receiver<T> {
+    channel: Arc<Channel<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn is_ready(&self) -> bool {
+        self.channel.ready.load(Relaxed)
+    }
+
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Acquire) {
+            panic!("no message available!");
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let a = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
+}
+
+fn main() {
+    thread::scope(|s| {
+        let (sender, receiver) = channel();
+        let t = thread::current();
+        s.spawn(move || {
+            sender.send("hello world!");
+            t.unpark();
+        });
+        while !receiver.is_ready() {
+            thread::park();
+        }
+        assert_eq!(receiver.receive(), "hello world!");
+    });
+}
---
+++ src/bin/05_one-shot_channel_b.rs
@@ -0,0 +1,80 @@
+use std::marker::PhantomData;
+use std::sync::atomic::Ordering::*;
+use std::thread::{self, Thread};
+use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicBool};
+
+struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
+        *self = Self::new();
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
+    }
+}
+
+pub struct Sender<'a, T> {
+    channel: &'a Channel<T>,
+    receiving_thread: Thread,
+}
+
+impl<T> Sender<'_, T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
+        self.receiving_thread.unpark();
+    }
+}
+
+pub struct Receiver<'a, T> {
+    channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>,
+}
+
+impl<T> Receiver<'_, T> {
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Acquire) {
+            thread::park();
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+
+fn main() {
+    let mut channel = Channel::new();
+    thread::scope(|s| {
+        let (sender, receiver) = channel.split();
+        s.spawn(move || {
+            sender.send("hello world!");
+        });
+        assert_eq!(receiver.receive(), "hello world!");
+    });
+}
