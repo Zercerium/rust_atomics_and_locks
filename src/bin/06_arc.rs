11fd5970e49ecf5e34b4fc2f24f2a3e490642f0b
Nick Kotsakidis <dev@zerc.dev>
2024-11-12 09:31:36 +0100

    chapter 6

--- src/bin/06_arc.rs
+++ src/bin/06_arc.rs
@@ -1,5 +1,7 @@
 use std::cell::UnsafeCell;
+use std::mem::ManuallyDrop;
 use std::sync::atomic::Ordering::*;
+use std::usize;
 use std::{
     ops::Deref,
     ptr::NonNull,
@@ -9,7 +11,7 @@ use std::{
 struct ArcData<T> {
     data_ref_count: AtomicUsize,
     alloc_ref_count: AtomicUsize,
-    data: UnsafeCell<Option<T>>,
+    data: UnsafeCell<ManuallyDrop<T>>,
 }
 
 pub struct Weak<T> {
@@ -39,7 +41,7 @@ impl<T> Weak<T> {
                 n = e;
                 continue;
             }
-            return Some(Arc { weak: self.clone() });
+            return Some(Arc { ptr: self.ptr });
         }
     }
 }
@@ -65,36 +67,64 @@ impl<T> Drop for Weak<T> {
 }
 
 pub struct Arc<T> {
-    weak: Weak<T>,
+    ptr: NonNull<ArcData<T>>,
 }
+
+unsafe impl<T: Sync + Send> Send for Arc<T> {}
+unsafe impl<T: Sync + Send> Sync for Arc<T> {}
 
 impl<T> Arc<T> {
     pub fn new(data: T) -> Arc<T> {
         Arc {
-            weak: Weak {
-                ptr: NonNull::from(Box::leak(Box::new(ArcData {
-                    alloc_ref_count: AtomicUsize::new(1),
-                    data_ref_count: AtomicUsize::new(1),
-                    data: UnsafeCell::new(Some(data)),
-                }))),
-            },
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                alloc_ref_count: AtomicUsize::new(1),
+                data_ref_count: AtomicUsize::new(1),
+                data: UnsafeCell::new(ManuallyDrop::new(data)),
+            }))),
         }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
     }
 
     pub fn downgrade(arc: &Self) -> Weak<T> {
-        arc.weak.clone()
+        let mut n = arc.data().alloc_ref_count.load(Relaxed);
+        loop {
+            if n == usize::MAX {
+                std::hint::spin_loop();
+                n = arc.data().alloc_ref_count.load(Relaxed);
+                continue;
+            }
+            assert!(n < usize::MAX - 1);
+            if let Err(e) =
+                arc.data()
+                    .alloc_ref_count
+                    .compare_exchange_weak(n, n + 1, Acquire, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Weak { ptr: arc.ptr };
+        }
     }
 
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
-        if arc.weak.data().alloc_ref_count.load(Relaxed) == 1 {
-            fence(Acquire);
-            let arcdata = unsafe { arc.weak.ptr.as_mut() };
-            let option = arcdata.data.get_mut();
-            let data = option.as_mut().unwrap();
-            Some(data)
-        } else {
-            None
+        if arc
+            .data()
+            .alloc_ref_count
+            .compare_exchange(1, usize::MAX, Acquire, Relaxed)
+            .is_err()
+        {
+            return None;
         }
+        let is_unique = arc.data().data_ref_count.load(Relaxed) == 1;
+        arc.data().alloc_ref_count.store(1, Release);
+        if !is_unique {
+            return None;
+        }
+        fence(Acquire);
+        unsafe { Some(&mut *arc.data().data.get()) }
     }
 }
 
@@ -102,27 +132,27 @@ impl<T> Deref for Arc<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        let ptr = self.weak.data().data.get();
-        unsafe { (*ptr).as_ref().unwrap() }
+        unsafe { &*self.data().data.get() }
     }
 }
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
-        let weak = self.weak.clone();
-        if weak.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+        if self.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
             std::process::abort();
         }
-        Arc { weak }
+        Arc { ptr: self.ptr }
     }
 }
 
 impl<T> Drop for Arc<T> {
     fn drop(&mut self) {
-        if self.weak.data().data_ref_count.fetch_sub(1, Release) == 1 {
+        if self.data().data_ref_count.fetch_sub(1, Release) == 1 {
             fence(Acquire);
-            let ptr = self.weak.data().data.get();
-            unsafe { (*ptr) = None }
+            unsafe {
+                ManuallyDrop::drop(&mut *self.data().data.get());
+            }
+            drop(Weak { ptr: self.ptr })
         }
     }
 }
